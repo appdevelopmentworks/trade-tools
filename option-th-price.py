184795e3284e0908cad73029f6b4fa6d4353dddc
appdevelopmentworks <81832175+appdevelopmentworks@users.noreply.github.com>
2025-03-05 22:12:25 +0900

    2nd commit

--- option-th-price.py
+++ option-th-price.py
@@ -4,6 +4,9 @@ import yfinance as yf
 from datetime import datetime, timedelta
 import streamlit as st
 from PIL import Image
+import seaborn as sns
+import matplotlib.pyplot as plt
+import japanize_matplotlib
 
 
 def black_scholes_european(S, K, sigma, r, T, option_type="call"):
@@ -147,10 +150,21 @@ def getHV(code):
     df.columns = [col[0] for col in df.columns]
     price_data = df["Close"] 
     return calculate_volatility(price_data, period=20).iloc[-1]
+
+#
+def plot_option_scatter(df, ticker, optyp, atm):
+    plt.figure(figsize=(10, 6))
+    sns.scatterplot(data=df, x='権利行使価格', y='終値-理論値', hue='出来高', size='出来高', sizes=(10, 500), markers=['s', 'o'])
+    plt.title(label=f"{ticker}の{optyp}オプションプレミアムと理論価格の差")
+    plt.axvline(x=int(atm), color='red', linestyle='--', label=f"ATM({atm})")
+    plt.legend()
+    return plt
+
 #####################################################################
 
 
 st.title("海外オプション理論価格ツール")
+st.text("理論価格との差を表示【アービトラージに使ってね！】")
 image = Image.open("headeroption.png")
 st.image(image)
 st.caption("ティッカーコードを入力し納会日、コールかプットを選択")
@@ -162,21 +176,26 @@ with col1:
     # Tickerオブジェクトの作成
     ticker = yf.Ticker(tcode)
     #
-    optionT = st.radio("オプションタイプ:", ("call", "put"), horizontal=True)
 with col2:
     gengetu = st.selectbox("限月を選択:", ticker.options)
-    redays = datetime.strptime(gengetu, "%Y-%m-%d") - datetime.now()
-    zandays = st.text_input("残存日数:", value=redays.days + 1)
+    optionT = st.radio("オプションタイプ:", ("call", "put"), horizontal=True)
     
 
-col3, col4, col5 = st.columns(3)
+col3, col4, col5, col6 = st.columns(4)
 
 with col3:
     gensiprice =  st.text_input("原資産価格:", value=ticker.info["regularMarketPrice"])
 with col4:
-    haitou = st.text_input("配当利回り:", value=ticker.info["dividendYield"])
-with col5:
     txtKinri = st.text_input("無リスク金利:", value=0.04478)
+with col5:
+    redays = datetime.strptime(gengetu, "%Y-%m-%d") - datetime.now()
+    zandays = st.text_input("残存日数:", value=redays.days + 1)
+with col6:
+    try:
+        divi = ticker.info["dividendYield"]
+    except:
+        divi = 0
+    haitou = st.text_input("配当利回り:", value=divi)
     
 
 #オプションのデータを取得
@@ -193,7 +212,7 @@ r = float(txtKinri)
 #S: 原資産価格
 S = ticker.info["regularMarketPrice"]
 #配当利回り
-dividend_yield = ticker.info["dividendYield"] / 100
+dividend_yield = divi / 100
 #残存日数
 redays = datetime.strptime(gengetu, "%Y-%m-%d") - datetime.now()
 T = (redays.days + 1) / 365
@@ -219,13 +238,18 @@ putsja.insert(2, '理論価格', rironchiP)
 putsja.insert(3, '終値-理論値', putsja["終値"] - putsja["理論価格"])
 
 st.write("HV(ヒストリカルボラティリティー)",round(sigma, 2))
+st.text(f"{optionT}オプション{gengetu}")
 # st.write("S",S)
 # st.write("T",T)
 # st.write("dividend_yield",dividend_yield)
 
 
 if optionT=="call":
+    fig = plot_option_scatter(callsja, tcode, optionT, S)
+    st.pyplot(fig)
     st.dataframe(callsja)
 else:
+    fig = plot_option_scatter(putsja, tcode, optionT, S)
+    st.pyplot(fig)
     st.dataframe(putsja)
 
